b73621fd3f7e45b091c3afde4dbd04df5684b29a
Praveensethuvel-27 <praveensethuvel27@gmail.com>
2026-07-18 10:48:35 +0530

    booking screen error cleared

--- user_app/lib/screens/bookings_screen.dart
+++ user_app/lib/screens/bookings_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import '../models/models.dart';
 import '../providers/auth_provider.dart';
 import '../providers/booking_provider.dart';
 import '../providers/language_provider.dart';
