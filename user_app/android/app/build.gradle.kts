bed4cba39381a6bd613d9870d1ac5a00a0dd75ea
isacnewton21 <isacnewton63@gmail.com>
2026-07-29 17:13:00 +0530

    test

--- user_app/android/app/build.gradle.kts
+++ user_app/android/app/build.gradle.kts
@@ -14,6 +14,7 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
+        isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
@@ -42,6 +43,7 @@ android {
 
 dependencies {
     implementation("androidx.core:core-splashscreen:1.0.1")
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
 
 flutter {
